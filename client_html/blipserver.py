7cacb0c7c9c327213f3a3c0abb36d30a198d2307
shaikhadil26 <m.adilshaikh26@gmail.com>
2024-08-12 21:57:28 +0530

    created frontend to visualize caption and latency

--- client_html/blipserver.py
+++ client_html/blipserver.py
@@ -1,9 +1,8 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, render_template
 import os
 import cv2
-import numpy as np
 import base64
-import torch
+import time
 from transformers import BlipProcessor, BlipForConditionalGeneration
 from PIL import Image
 
@@ -16,36 +15,34 @@ model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-capt
 
 i = 0
 
-@app.route('/process', methods=['POST'])
-def process_data():
-    data = request.json
+@app.route('/', methods=['GET', 'POST'])
+def upload_image():
     global i
-    if 'image' not in data:
-        return jsonify({'error': 'No image data provided'}), 400
-
-    image_data = data['image']
-    try:
-        image_data = base64.b64decode(image_data)
-        np_arr = np.frombuffer(image_data, np.uint8)
-        image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+    if request.method == 'POST':
+        file = request.files['image']
+        if not file:
+            return "No file uploaded", 400
 
         i += 1
         file_path = os.path.join(save_folder, f'received_image{i}.jpg')
-        cv2.imwrite(file_path, image)
-        if image is None:
-            raise ValueError("Failed to decode image")
+        file.save(file_path)
 
+        image = cv2.imread(file_path)
         image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         pil_image = Image.fromarray(image_rgb)
+
         inputs = processor(pil_image, return_tensors="pt")
+        start_time = time.time()
         outputs = model.generate(**inputs)
+        processing_time = time.time() - start_time
         caption = processor.decode(outputs[0], skip_special_tokens=True)
 
-        return jsonify({'message': 'Image processed successfully', 'caption': caption})
+        _, buffer = cv2.imencode('.jpg', image)
+        image_encoded = base64.b64encode(buffer).decode('utf-8')
 
-    except Exception as e:
-        print(f"Error processing image: {e}")
-        return jsonify({'error': 'Failed to process image'}), 500
+        return render_template('index.html', image_data=image_encoded, caption=caption, processing_time= processing_time)
+
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
